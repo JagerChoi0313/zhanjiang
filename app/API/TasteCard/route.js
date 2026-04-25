23e128c81d6e07f7cd616840b3d998c30b317ad4
JagerChoi <17322905680@qq.com>
2026-04-25 10:59:55 +0800

    "味觉卡片"后端的初步实现

--- app/API/TasteCard/route.js
+++ app/API/TasteCard/route.js
@@ -2,4 +2,20 @@ import {db} from '../../../database/index'
 import {TasteCard} from '../../../database/schema'
 import {NextResponse} from 'next/server'
 
-//我先下班吃饭
+export async function GET(){
+
+    try{
+        //使用drizzle查询食材
+        const data = await db.select()
+        .from(TasteCard)
+
+        //返回JSON数据
+        return NextResponse.json(data);
+    }catch(error){
+        console.error("读取食材数据库失败：",error)
+        return NextResponse.json(
+            {error:"Internal Server Error"},
+            {status:500}
+        );
+    }
+}
